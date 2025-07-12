55a9a25d77ac5cef504431db7f9425b5646f0d64
Dim Shvydkoy <dmit.shvyd@gmail.com>
2025-07-12 13:16:21 +0000

    Make Utils.runCommand() more convenient.

--- ProjectRoot/CMagneto/py/utils.py
+++ ProjectRoot/CMagneto/py/utils.py
@@ -96,35 +96,48 @@ class Utils(metaclass=ConstMetaClass):
 
     @staticmethod
     def runCommand(iCommand: list[str], iCWD: Path | None = None, *, iCheck: bool = True, iCaptureOutput: bool = False) -> subprocess.CompletedProcess | None:
-        currentCWD: str = os.getcwd()
+        currentCWD = os.getcwd()
         if iCWD is not None:
             os.chdir(iCWD)
 
-        print(Utils.makeColored(f"{Utils.__LOG_MESSAGE_PREFIX}Running command: ", Utils.PrintColor.Cyan) + \
-              Utils.makeColored(f"{os.getcwd()}> ", Utils.PrintColor.Magenta) + \
-              Utils.makeColored(shlex.join(iCommand), Utils.PrintColor.Blue),
-              flush=True
+        print(
+            Utils.makeColored("Running command: ", Utils.PrintColor.Cyan) + \
+            Utils.makeColored(f"{os.getcwd()}> ", Utils.PrintColor.Magenta) + \
+            Utils.makeColored(shlex.join(iCommand), Utils.PrintColor.Blue),
+            flush=True
         )
 
         try:
-            output = subprocess.run(
+            process = subprocess.Popen(
                 iCommand,
-                check=iCheck,
-                capture_output=True,   # Always capture so you can print stderr on failure
-                text=True
+                stdout=subprocess.PIPE,   # Stream output line-by-line.
+                stderr=subprocess.STDOUT, # Merge `stderr` into `stdout` to preserve order.
+                text=True,                # Convert bytes into text to render escaped characters, etc.
+                bufsize=1,                # Enable line buffering for real-time printing.
+                universal_newlines=True,  # Interpret `\r\n`, etc. `\n`.
             )
-            if iCaptureOutput:
-                return output
-            else:
-                print(output.stdout, end='')
-                print(output.stderr, end='')
-                return None
+            assert process.stdout is not None # For type checker.
 
-        except subprocess.CalledProcessError as e:
-            print(e.stdout or "", end='')
-            Utils.printColored(f"{Utils.__LOG_MESSAGE_PREFIX}Command failed with error:", Utils.PrintColor.Red)
-            print(e.stderr or "", end='')
-            raise
+            capturedLines = []
+            for line in process.stdout:
+                print(line, end='') # Print from the sub process `stdout` stream in real time. Each line already has a endline.
+                if iCaptureOutput:
+                    capturedLines.append(line) # Output is requested. Save captured lines and return them later as a batch placed inside a `CompletedProcess` entity.
+
+            returnCode = process.wait()
+            if iCheck and returnCode != 0:
+                Utils.printColored(f"{Utils.__LOG_MESSAGE_PREFIX}Command failed with error.", Utils.PrintColor.Red)
+                raise subprocess.CalledProcessError(returnCode, iCommand, output="".join(capturedLines))
+
+            if iCaptureOutput:
+                return subprocess.CompletedProcess(
+                    args=iCommand,
+                    returncode=returnCode,
+                    stdout="".join(capturedLines),
+                    stderr=None
+                )
+            else:
+                return None
 
         finally:
             if iCWD is not None:
