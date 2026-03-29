da8cbf39de234e2be8ef1c18e79e562e0941f920
Dim Shvydkoy <dmit.shvyd@gmail.com>
2026-03-29 05:23:55 +0000

    Add missing license headers and remove year from them.

--- SeedProject/src/Dish/ContactHolder/Contacts/fields/PhoneNumber.cpp
+++ SeedProject/src/Dish/ContactHolder/Contacts/fields/PhoneNumber.cpp
@@ -1,3 +1,10 @@
+// Copyright (c) Dmitrii Shvydkoi ("Dim Shvydkoy")
+// SPDX-License-Identifier: MIT
+//
+// This file is part of the ContactHolder project.
+// The MIT license text is available in the LICENSE file
+// located at the root directory of the project.
+
 #include "PhoneNumber.hpp"
 
 #include "PhoneNumberFormatter/Formatter.hpp"
