5ad7759149db2e5def84cc3f2ad90a85217db376
Dim Shvydkoy <dmit.shvyd@gmail.com>
2026-04-10 12:40:38 +0000

    Remove junk.

--- SeedProject/CMagneto/cpp/sources/include/CMagneto/Core/FrameworkInfo.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string_view>
-
-
-namespace CMagneto::Core {
-    [[nodiscard]] std::string_view frameworkName() noexcept;
-} // namespace CMagneto::Core
--- SeedProject/CMagneto/cpp/sources/include/CMagneto/Qt/FrameworkInfo.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <QString>
-
-
-namespace CMagneto::Qt {
-    [[nodiscard]] QString frameworkName();
-} // namespace CMagneto::Qt
